ef3e94dabd6546a1e4339e2723fd810879a34374
nirman11 <158264832+nirman11@users.noreply.github.com>
2024-01-31 11:21:02 +0200

    Update add.py

--- add.py
+++ add.py
@@ -5,6 +5,6 @@ This is a module to help you add two numbers.
 
 def add(argument_a, argument_b):
     """
-    This function adds two numbers
+    This function adds two numbers.
     """
     return argument_a+argument_b
